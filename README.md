3e28601846ceb5017fe7b6acafe570d1f5d1a81d
manbalboy <manbalboy@hanmail.net>
2021-04-10 09:09:52 +0900

    babel 을 이용한 jsx 변환

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 ## JSX 
 
 ## BABEL
+```
+D:\study\react\startReact\hello-world\simple1.js
 
+createElement 를 jsx 로 변환하기위해 babel 로 작업을 해준다. 
+
+npm init 
+
+npm i @babel/core @babel/cli @babel/preset-react -D
+
+# 설치 이후에 다음명령어를 입력 하여 바벨을 실행한다 .
+
+src 폴더를  만든후 이동 
+
+npx babel --watch src --out-dir . --presets @babel/preset-react 
+
+```
 
 ## Webpack
--- hello-world/simple1.js
+++ hello-world/simple1.js
@@ -1,58 +1,45 @@
 function LikeButton() {
-    const [liked, setLiked] = React.useState(false);
-    const text = liked ? '좋아요 취소' : '좋아요';
-    return <button onClick={() => setLiked(!liked)}>(text)</button>;
-    // React.createElement(
-    //     'button',
-    //     { onClick: () => setLiked(!liked) },
-    //     text
-    // );
+  const [liked, setLiked] = React.useState(false);
+  const text = liked ? '좋아요 취소' : '좋아요';
+  return /*#__PURE__*/React.createElement("button", {
+    onClick: () => setLiked(!liked)
+  }, text); // React.createElement(
+  //     'button',
+  //     { onClick: () => setLiked(!liked) },
+  //     text
+  // );
 }
 
-
 function Container() {
-    const [count, setCount] = React.useState(0);
-    return (
-        <div>
-            <LikeButton />
-            <div>
-                <span>현재카운트 : </span>
-                <span style={{ marginRight: 10 }}>(count)</span>
-                <button onClick={() => setCount(count + 1)}>증가</button>;
-                <button onClick={() => setCount(count - 1)}>감소</button>;
-            </div>
-        </div>
-    );
-    // React.createElement(
-    //     'div',
-    //     null,
-    //     React.createElement(LikeButton),
-    //     React.createElement('div', { style: { marginTop: 20 } },
-    //         React.createElement('span', null, '현재카운트'),
-    //         React.createElement('span', { style: { marginRight: 10 } }, count),
-    //         React.createElement('button', { onClick: () => setCount(count + 1) }, '증가'),
-    //         React.createElement('button', { onClick: () => setCount(count - 1) }, '감소'),
-    //     )
-    // )
+  const [count, setCount] = React.useState(0);
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(LikeButton, null), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("span", null, "\uD604\uC7AC\uCE74\uC6B4\uD2B8 : "), /*#__PURE__*/React.createElement("span", {
+    style: {
+      marginRight: 10
+    }
+  }, count), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count + 1)
+  }, "\uC99D\uAC00"), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count - 1)
+  }, "\uAC10\uC18C"))); // React.createElement(
+  //     'div',
+  //     null,
+  //     React.createElement(LikeButton),
+  //     React.createElement('div', { style: { marginTop: 20 } },
+  //         React.createElement('span', null, '현재카운트'),
+  //         React.createElement('span', { style: { marginRight: 10 } }, count),
+  //         React.createElement('button', { onClick: () => setCount(count + 1) }, '증가'),
+  //         React.createElement('button', { onClick: () => setCount(count - 1) }, '감소'),
+  //     )
+  // )
 }
 
 const domContainer = document.getElementById('root');
-
-ReactDOM.render(
-    React.createElement(Container), domContainer
-);
+ReactDOM.render(React.createElement(Container), domContainer);
 const domContainer1 = document.getElementById('root1');
-
-ReactDOM.render(
-    React.createElement('div', null, [
-        React.createElement('p', { key: 'aa' }, 'hello'),
-        React.createElement('p', { key: 'aa1' }, 'hello1')]), domContainer1
-);
+ReactDOM.render(React.createElement('div', null, [React.createElement('p', {
+  key: 'aa'
+}, 'hello'), React.createElement('p', {
+  key: 'aa1'
+}, 'hello1')]), domContainer1);
 const domContainer2 = document.getElementById('root2');
-
-ReactDOM.render(
-    React.createElement(LikeButton), domContainer2
-);
-
-
-
+ReactDOM.render(React.createElement(LikeButton), domContainer2);
