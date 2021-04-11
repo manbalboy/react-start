1116cf81a882a4683fd10457bc4c1944aa319c18
manbalboy <manbalboy@hanmail.net>
2021-04-11 19:29:06 +0900

    css module

--- css-import/src/Button.js
+++ css-import/src/Button.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './Button.css';
+// import './Button.css';
+import Style from'./Button.module.css';
 
 export default function Button({ size }) {
     if (size == 'big') {
-        return <button className="button big"> 큰버튼 </button>;
+        return <button className={`${Style.button} ${Style.big}`}> 큰버튼 </button>;
     } else {
-        return <button className="button small"> 작은버튼 </button>;
+        return <button className={`${Style.button} ${Style.small}`}> 작은버튼 </button>;
     }
 }
